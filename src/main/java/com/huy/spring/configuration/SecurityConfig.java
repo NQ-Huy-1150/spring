037f4d40dc7e53c8925fb98eb45a1c579045faef
Nguyen Quang Huy <quanghuy190105@gmail.com>
2026-07-04 13:34:20 +0700

    feat: add security configuration for JWT authentication and public endpoint access

---
+++ src/main/java/com/huy/spring/configuration/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.huy.spring.configuration;
+
+import com.nimbusds.jose.JWSAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Value("${jwt.signerKey}")
+    private String signerKey;
+    private final String[] PUBLIC_POST_ENDPOINTS = {"/auth/login","/api/auth/create","/auth/introspect"};
+    private final String[] PUBLIC_GET_ENDPOINTS = {"/api/auth/users"};
+    private final String[] PUBLIC_PUT_ENDPOINTS = {"/api/auth/update"};
+    private final String[] PUBLIC_DELETE_ENDPOINTS = {"/api/auth/delete"};
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(request ->
+                request.requestMatchers(HttpMethod.GET, PUBLIC_GET_ENDPOINTS ).permitAll()
+                        .requestMatchers(HttpMethod.POST,PUBLIC_POST_ENDPOINTS).permitAll()
+                        .requestMatchers(HttpMethod.PUT,PUBLIC_PUT_ENDPOINTS).permitAll()
+                        .requestMatchers(HttpMethod.DELETE,PUBLIC_DELETE_ENDPOINTS).permitAll()
+                .anyRequest().authenticated());
+        httpSecurity.oauth2ResourceServer(oauth2 ->
+                oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder())));
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        return httpSecurity.build();
+    }
+    @Bean
+    JwtDecoder jwtDecoder(){
+        SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS256");
+        return NimbusJwtDecoder.withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS256)
+                .build();
+    }
+}
